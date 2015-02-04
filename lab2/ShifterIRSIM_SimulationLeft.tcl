4381506c10aa85aa68746679ec1b6e55e806ead1
griffingarland <griffin.garland@gmail.com>
2015-02-04 16:21:45 -0500

    Added two IRSIM simulations for the Shifter

---
+++ lab2/ShifterIRSIM_SimulationLeft.tcl
@@ -0,0 +1,1 @@
+l gnd; h shl; stepsize 100; w shl notsl FiPlus1 Fi_1 RAMINi Fi; vector in Fi_1 Fi; set vlist {00 01 10 11}; foreach vec $vlist {setvector in $vec; s}; analyzer shl notshl fi_1 fi ramini fiplus1
